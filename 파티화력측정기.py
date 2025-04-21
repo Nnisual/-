f9f4309cf32f5a9c8c690b99c9c4b6893b73916d
Nnisual <emd94@naver.com>
2025-04-21 19:38:02 +0900

    기능수정

--- 파티화력측정기.py
+++ 파티화력측정기.py
@@ -69,14 +69,32 @@ class dnfCalc:
 #     main()
 
 
+
+
 st.title("던파 파티 화력 측정기")
+st.markdown("""
+파티 내 딜러들의 딜량과 버퍼의 버프력을 바탕으로  
+**실제 적용된 총 파티 화력**을 계산해줍니다.
+※ 400버퍼 기준으로 작성되었습니다.            
+""")
+
+
+
+
 
 buf_name = st.selectbox("버퍼 타입을 선택하세요", ["헤카테", "뮤즈", "크루세이더(여)","크루세이더(남)"])
+
+
 buf_str = st.number_input("버프력 (예: 400)", value=400.0)
+st.caption("※ 헤카테의 버프력은 던담 4인 기준입니다.")
 
 dealer1 = st.number_input("딜러 1 딜량", value=30.0)
 dealer2 = st.number_input("딜러 2 딜량", value=30.0)
 dealer3 = st.number_input("딜러 3 딜량", value=30.0)
+
+dft = 90
+
+
 
 if st.button("계산하기"):
     model = dnfCalc(buf_name, dealer1, dealer2, dealer3)
@@ -86,8 +104,10 @@ if st.button("계산하기"):
     for i, val in enumerate(model.new_party, 1):
         st.write(f"딜러{i}: {val}")
     st.success(f"총 파티 화력: {result}")
-
-
+    
+    st.caption("30/400 파티 기준")
+    st.success(f"{(result / dft) * 100:.1f}%")
+    
 
 
 
